1cedf4fa57449eb24f1b9f131f6c16cc97755ed9
HarukiMatsushita <ac.haruki.matsushita@gmail.com>
2026-05-14 09:11:40 +0900

    shadcn/ui追加

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import "./styles.css";
 
 function ReactBridge() {
+  void Button;
+  void Card;
   return null;
 }
 
